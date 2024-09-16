268e7fe575d5fac33ebfb950e1f2170507e18aa9
Dominic A.N. <dominicnunez@gmail.com>
2024-09-16 13:15:12 -0500

    Update BusRoute model to be more verbose.

--- internal/model/route.go
+++ internal/model/route.go
@@ -3,7 +3,7 @@ package model
 // School struct represents a single school
 // BusRoute struct represents a single bus route
 type BusRoute struct {
-	ID       int    `json:"id"`
+	RouteID  int    `json:"route_id"`
 	SchoolID int    `json:"school_id"`
 	Students []int  `json:"student_ids"`
 	Stops    []Stop `json:"stops"`
